a7af93a20aeb3a04c08c938adae7308452da2267
Коля Васильев <nikolayvas20@gmail.com>
2020-12-18 18:38:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            double x = double.Parse(Console.ReadLine());
+            const double b = 0.4;
+            double a = Math.Log10(x);
+            double c = Math.Pow(a, 2) + Math.Sqrt(b * x);
+            double y = Math.Pow(Math.E, 2 * x) + Math.Pow(9.7, c);
+            Console.WriteLine("a = "+ a +  " c = " + c + " y = " +y);
+            Console.ReadKey();
+
+        }
+    }
+}
